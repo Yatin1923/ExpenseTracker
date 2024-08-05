f730be2ef723820be486bb54f3417a3ae99f3674
Ankur <ankur.mekvan@allata.com>
2024-08-05 15:41:11 +0530

    create set pin and signIn success page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import Onboarding from "./pages/onboarding/Onboarding.tsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./pages/onboarding/signup/Signup.tsx";
 import React from "react";
+import SetPin from "./pages/onboarding/set-pin/Setpin.tsx";
+import SignupSuccess from "./pages/onboarding/signUp-success/SignupSuccess.tsx";
+// import SetPin from "./pages/onboarding/set-pin/SetPin.tsx";
 function App() {
   return (
     <div className="App">
@@ -10,6 +13,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Onboarding />}></Route>
           <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/set-pin" element={<SetPin />}></Route>
+          <Route path="/signup-success" element={<SignupSuccess />}></Route>
         </Routes>
       </Router>
     </div>
