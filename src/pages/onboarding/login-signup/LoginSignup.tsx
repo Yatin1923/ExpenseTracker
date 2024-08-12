c446d0c87d0d44dd69c9211741eddacae789593a
SChaudhar4 <Swapnil.Chaudhari1@cbre.com>
2024-08-12 17:32:38 +0530

    replace react-swipeabl-view with swiper

--- src/pages/onboarding/login-signup/LoginSignup.tsx
+++ src/pages/onboarding/login-signup/LoginSignup.tsx
@@ -1,33 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { Box, Typography, Button, Container } from "@mui/material";
-import SwipeableViews from "react-swipeable-views";
 import slideImg1 from "../../../images/onboarding/Onboarding-img1.png";
 import slideImg2 from "../../../images/onboarding/Onboarding-img2.png";
-
 import slideImg3 from "../../../images/onboarding/Onboarding-img3.png";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
+import { Autoplay ,Navigation, Pagination} from 'swiper/modules';
 import { useNavigate } from 'react-router-dom';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 const LoginSignup = () => {
-  const [index, setIndex] = useState(0);
   const [showMain, setShowMain] = useState(false);
   const navigate = useNavigate();
-
-  const handleChangeIndex = (index) => {
-    setIndex(index);
-  };
-
+  
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowMain(true);
-    }, 3000); // Display "Welcome to Our App!" for 5 seconds
+    }, 3000); // Display "Welcome to Our App!" for 3 seconds
     return () => clearTimeout(timer);
-  }, []);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setIndex((prevIndex) => (prevIndex + 1) % 3);
-    }, 3000); // Change every 3 seconds
-    return () => clearInterval(interval);
   }, []);
 
   const slides = [
@@ -39,12 +31,12 @@ const LoginSignup = () => {
     {
       title: "Know where your money goes",
       subtitle:
-        "Track your transaction easily,with categories and financial report",
+        "Track your transaction easily, with categories and financial report",
       image: slideImg2,
     },
     {
-      title: "planning ahead",
-      subtitle: "Setup your budget for each category so you in control",
+      title: "Planning ahead",
+      subtitle: "Setup your budget for each category so you are in control",
       image: slideImg3,
     },
   ];
@@ -52,6 +44,7 @@ const LoginSignup = () => {
   const handleSignUpClick = () => {
     navigate('/signup');
   };
+  
   const handleLoginClick = () => {
     navigate('/login');
   };
@@ -62,106 +55,90 @@ const LoginSignup = () => {
         textAlign: "center",
         width: "100vw",
         height: "100vh",
-        backgroundColor: "#FFF", // Ensure background color is white
-        opacity: 1, // Fade in/out effect
-        transform: showMain ? "translateY(0)" : "translateY(0px)", // Slide effect issue of margin on home page here
-        transition: "transform 0.3s ease-in-out", // Smooth slide-in
+        backgroundColor: "#FFF", 
         display: "flex",
         flexDirection: "column",
-        alignItems: "center", // Center horizontally
-        justifyContent: "center", // Center vertically
-        margin: "0px !important",
-        padding: "0px !important",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "0 !important",
+        margin: "0 !important",
       }}
     >
       {showMain ? (
         <>
-          <Container>
-            <SwipeableViews index={index} onChangeIndex={handleChangeIndex}>
-              {slides.map((slide, i) => (
-                <Box key={i} sx={{ padding: 2, mt: 5 }}>
-                  <img
-                    src={slide.image}
-                    alt={`slide-${i}`}
-                    style={{
-                      width: "100%",
-                      height: "auto",
-                      paddingBottom: "5vh",
-                    }}
-                  />
-                  <Typography
-                    variant="h5"
-                    gutterBottom
-                    sx={{ fontSize: "32px", fontWeight: "bold" }}
-                  >
-                    {slide.title}
-                  </Typography>
-                  <Typography variant="body1" sx={{ color: "#91919F" }}>
-                    {slide.subtitle}
-                  </Typography>
-                  
-                </Box>
-              ))}
-            </SwipeableViews>
-            <Box
-                    sx={{
-                      display: "flex",
-                      justifyContent: "center",
-                      mt: 2,
-                      alignItems: "center",
-                    }}
-                  >
-                    {slides.map((_, dotIndex) => (
-                      <Box
-                        key={dotIndex}
-                        sx={{
-                          width: dotIndex === index ? 15 : 10,
-                          height: dotIndex === index ? 15 : 10,
-                          borderRadius: "50%",
-                          backgroundColor:
-                            dotIndex === index ? "#7F3DFF" : "grey.400",
-                          mx: 0.5,
-                          transition: "background-color 0.5s ease-in-out",
-                        }}
-                      />
-                    ))}
-                  </Box>
-            <Box
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                width: "85vw",
-                padding: "20px",
-              }}
+          <Box sx={{ width: "100%", flex: 1 }}>
+            <Swiper
+              modules={[Autoplay,Navigation, Pagination]}
+              spaceBetween={50}
+              slidesPerView={1}
+              autoplay={{ delay: 2000 }} // Autoplay with 3 seconds interval
+              loop={true} // Loop through slides
             >
-              <Button
-                variant="contained"
-                sx={{
-                  mb: 2,
-                  background: "#7F3DFF",
-                  height: "56px",
-                  borderRadius: "16px",
-                }}
-                onClick={handleSignUpClick}
-              >
-                Sign Up
-              </Button>
-              <Button
-                variant="outlined"
-                sx={{
-                  mb: 2,
-                  background: "#EEE5FF",
-                  height: "56px",
-                  color: "#7F3DFF",
-                  border: "1px solid #EEE5FF",
-                  borderRadius: "16px",
-                }}
-                onClick={handleLoginClick}
-              >
-                Login
-              </Button>
-            </Box>
-          </Container>
+              {slides.map((slide, i) => (
+                <SwiperSlide key={i}>
+                  <Box sx={{ padding: 2, textAlign: 'center' }}>
+                    <img
+                      src={slide.image}
+                      alt={`slide-${i}`}
+                      style={{
+                        width: "100%",
+                        height: "auto",
+                        maxWidth: "600px",
+                        margin: "0 auto", // Center image
+                      }}
+                    />
+                    <Typography
+                      variant="h5"
+                      gutterBottom
+                      sx={{ fontSize: "32px", fontWeight: "bold", marginTop: "20px" }}
+                    >
+                      {slide.title}
+                    </Typography>
+                    <Typography variant="body1" sx={{ color: "#91919F" }}>
+                      {slide.subtitle}
+                    </Typography>
+                  </Box>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </Box>
+
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              width: "85vw",
+              padding: "20px",
+              marginTop: "auto", 
+              marginBottom: "40px", 
+            }}
+          >
+            <Button
+              variant="contained"
+              sx={{
+                mb: 2,
+                background: "#7F3DFF",
+                height: "56px",
+                borderRadius: "16px",
+              }}
+              onClick={handleSignUpClick}
+            >
+              Sign Up
+            </Button>
+            <Button
+              variant="outlined"
+              sx={{
+                background: "#EEE5FF",
+                height: "56px",
+                color: "#7F3DFF",
+                border: "1px solid #EEE5FF",
+                borderRadius: "16px",
+              }}
+              onClick={handleLoginClick}
+            >
+              Login
+            </Button>
+          </Box>
         </>
       ) : (
         <Box
@@ -175,8 +152,8 @@ const LoginSignup = () => {
             color: "#fff",
             transition: "opacity 1s ease-in-out",
             opacity: 1,
-            margin: 0, // Remove default margin
-            padding: 0, // Remove default padding
+            margin: 0, 
+            padding: 0, 
           }}
         >
           <Typography sx={{ fontWeight: "bold", fontSize: "56px" }}>
